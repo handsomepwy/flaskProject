239d8e9e59115e641e3ce466e08fe36a6298883c
潘文渊 <peterrabbit2009>
2022-07-11 19:15:44 +0800

    doge 1.0 real

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -10,8 +9,6 @@ user_list = ["pwy", "ybn", "hzh", "gzr", "pwy", "dxm"]
 @app.route("/")
 def hello_world():
     return render_template("main.html")
-def sign():
-    return render_template("sign.html")
 
 
 @app.route("/<path_name>/")
@@ -23,10 +20,14 @@ def not_found(path_name):
 def hello(search_name):
     for c in user_list:
         if c == search_name:
-            return render_template("user.html", search_name=search_name, return_material="oh! " + search_name + " is very smart!")
+            return render_template("user.html", search_name=search_name, return_material="oh! " + search_name + "is "
+                                                                                                                "very"
+                                                                                                                "smart!"
+                                   )
     else:
-        return render_template("user.html", search_name=search_name, return_material="oops, I don't have any profile of " + search_name)
+        return render_template("user.html", search_name=search_name, return_material="oops, I don't have any profile "
+                                                                                     "of " + search_name)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
