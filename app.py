e9169436ece2a749a7a3ac9d4973b0e5a314d8e2
潘文渊 <peterrabbit2009>
2022-07-29 14:25:38 +0800

    manual human record(vis.txt)'s writing format is improved

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import time
 import sqlite3
 connection = sqlite3.connect("test.db")
@@ -16,37 +16,38 @@ res_html = res_html + "</ul>"
 app = Flask(__name__)
 
 
-user_list = ["pwy", "ybn", "hzh", "gzr", "pwy", "dxm"]
+user_list = ["pwy", "ybn", "hzh", "gzr"]
 
 
 @app.route("/")
 def hello_world():
-    f = open("vis.txt", "a")
     localtime = time.asctime(time.localtime(time.time()))
-    f.write("visited / page on " + localtime + "\n")
-    return render_template("main.html", com = res_html)
+    f = open("vis.txt", "a")
+    ip = request.remote_addr
+    f.write("{ip} visited {path} page on {vis_time}\n".format(ip=ip, path="index", vis_time=localtime))
+    return render_template("main.html", com=res_html)
 
 
 @app.route("/<path_name>/")
 def not_found(path_name):
-    f = open("vis.txt", "a")
     localtime = time.asctime(time.localtime(time.time()))
-    f.write("visited " + path_name + " page on " + localtime + "\n")
+    f = open("vis.txt", "a")
+    ip = request.remote_addr
+    f.write("{ip} visited {path} page on {vis_time}\n".format(ip=ip, path=path_name, vis_time=localtime))
     return render_template("not_found.html", path_name=path_name)
 
 
 @app.route("/user/<search_name>/")
 def hello(search_name):
-    f = open("vis.txt", "a")
     localtime = time.asctime(time.localtime(time.time()))
+    f = open("vis.txt", "a")
+    ip = request.remote_addr
+    f.write("{ip} visited user {username} page on {vis_time}\n".format(ip=ip, username=search_name, vis_time=localtime))
     for c in user_list:
         if c == search_name:
-            f.write("visited user " + search_name + " page on " + localtime + "\n")
             return render_template("user.html", search_name=search_name,
                                    return_material="oh! "+search_name+" is very smart!")
-    f.write("visited user " + search_name + " page on " + localtime + "\n")
-    return render_template("user.html", search_name=search_name,
-                           return_material="oops, I don't have any profile of " + search_name)
+    return render_template("user.html", search_name=search_name, return_material="oops, I don't have any profile of " + search_name)
 
 
 if __name__ == '__main__':
